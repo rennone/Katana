71ee8653df9621d6e154a6e0f2390de4565a138a
Ren Mizutani <nightfall-immortal-longview@live.jp>
2015-10-18 21:21:25 +0900

    Revert "Revert "scriptsの作成""
    This reverts commit 59a47bf9d40a4d28990b3550b01f12930d1b9ef6.

---
+++ Katana/Assets/Developers/Nashiko/scripts/DummyPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DummyPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // left
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            this.transform.position += Vector3.left;
+        }
+        // right
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.position += Vector3.right;
+        }
+    }
+}
