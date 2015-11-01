924dfc850923ccf7c5566140e1c93575deee52c9
nashik <nashiko1215@gmail.com>
2015-11-01 18:15:58 +0900

    title遷移を追加

---
+++ Katana/Assets/Scripts/Title.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Title : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			Application.LoadLevel("TestPlayerScene");
+		}
+	}
+}
