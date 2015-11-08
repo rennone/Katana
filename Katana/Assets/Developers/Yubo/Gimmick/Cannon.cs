862bced30ce35f735557a928bd2b23c3ffe23c6f
Yuki Kono <yuboyubo919@gmail.com>
2015-11-09 00:51:47 +0900

    大砲ギミックの大枠を作成（範囲内に入ったターゲットに銃口を向けて、一定間隔で玉を発射するようにした）

---
+++ Katana/Assets/Developers/Yubo/Gimmick/Cannon.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cannon : MonoBehaviour {
+
+    public CannonBullet bulletPrefab;
+    public Transform bulletPos;
+    public float bulletMergin = 2f;
+    public float bulletSpeed = 10f;
+    public float searchDistance = 20f;
+
+    [SerializeField]
+    private Transform target;
+
+    private bool canFire = true;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float dist = (target.position - this.transform.position).magnitude;
+        if (dist < searchDistance)
+        {
+            this.transform.LookAt(target.position);
+
+            if (!canFire)
+                return;
+
+            canFire = false;
+            CannonBullet bullet = Instantiate(bulletPrefab, bulletPos.position, Quaternion.identity) as CannonBullet;
+            Vector3 targetDir = (target.position - this.transform.position).normalized;
+            bullet.Fire(targetDir,bulletSpeed);
+            Invoke("SetCanFire", bulletMergin);
+        }
+    }
+
+    void SetCanFire()
+    {
+        canFire = true;
+    }
+}
---
+++ Katana/Assets/Developers/Yubo/Gimmick/CannonBullet.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonBullet : MonoBehaviour {
+
+    bool canMove = false;
+    Vector3 direction = Vector3.zero;
+    float speed = 0;
+
+    public void Fire(Vector3 targetDir, float bulletSpeed)
+    {
+        canMove = true;
+        direction = targetDir;
+        speed = bulletSpeed;
+    }
+
+    void Update()
+    {
+        if (!canMove)
+            return;
+
+        Vector3 newPos = this.transform.position + direction * speed * Time.deltaTime;
+        this.transform.position = newPos;
+    }
+}
