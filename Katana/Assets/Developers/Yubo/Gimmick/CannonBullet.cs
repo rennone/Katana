7090f14cf1ad8c4206ad6ecc283b9a6ec13f6c79
Yuki Kono <yuboyubo919@gmail.com>
2015-11-14 23:15:06 +0900

    いらないフラグを消した

--- Katana/Assets/Developers/Yubo/Gimmick/CannonBullet.cs
+++ Katana/Assets/Developers/Yubo/Gimmick/CannonBullet.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class CannonBullet : MonoBehaviour {
 
-    bool canMove = false;
     Vector3 direction = Vector3.zero;
     float speed = 0;
     Vector3 firstPos = Vector3.zero;
@@ -11,7 +10,6 @@ public class CannonBullet : MonoBehaviour {
 
     public void Fire(Vector3 targetDir, float bulletSpeed,float explosionDistance)
     {
-        canMove = true;
         direction = targetDir;
         speed = bulletSpeed;
         firstPos = this.transform.position;
@@ -20,9 +18,6 @@ public class CannonBullet : MonoBehaviour {
 
     void Update()
     {
-        if (!canMove)
-            return;
-
         Vector3 newPos = this.transform.position + direction * speed * Time.deltaTime;
         this.transform.position = newPos;
         if((newPos - firstPos).magnitude > expDist)
