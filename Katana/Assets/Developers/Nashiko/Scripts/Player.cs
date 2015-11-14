6ac77b4fe72bdcc9fca8ca2a665276c02d2bd07e
nashik <nashiko1215@gmail.com>
2015-11-14 22:16:26 +0900

    add Stage4.unity

---
+++ Katana/Assets/Developers/Nashiko/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	
+	public float JumpInitialVelocity;
+	public float HorizontalInitialVelocity;
+
+	private bool isJumpping = false;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//左右の移動
+		this.transform.position += (HorizontalInitialVelocity * Input.GetAxisRaw("Horizontal")) * Vector3.right;
+
+		//ジャンプ
+		if (Input.GetButtonDown ("Jump") && !isJumpping) {
+			isJumpping = true;
+			jump ();
+		}
+	}
+
+	void OnCollisionEnter(Collision collision){
+//		if (collider.gameObject.name () == "Terrain") {
+		Debug.Log("test");
+   		isJumpping = false;
+//		}
+	}
+
+	private void jump(){
+		var rigitbody = GetComponent<Rigidbody>();
+		rigitbody.AddForce (Vector3.up * JumpInitialVelocity);
+	}
+}
