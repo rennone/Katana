ff62db20c68b7ebb864a5683463bb6af8ffc27d5
Ren Mizutani <nightfall-immortal-longview@live.jp>
2015-12-20 21:30:22 +0900

    デバッグメニューの基本機能の実装。

--- Katana/Assets/Scripts/System/GameManager.cs
+++ Katana/Assets/Scripts/System/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 
 public class GameManager : Singleton<GameManager> {
@@ -6,13 +7,51 @@ public class GameManager : Singleton<GameManager> {
     [HideInInspector]
     public string nowSceneName;
 
+    private DebugViewer debugCanvas_;
+
     void Start()
     {
         nowSceneName = Application.loadedLevelName;
+
+        GameObject canvas = GameObject.Instantiate(Resources.Load("Prefab/DebugMenu/DebugCanvas")) as GameObject;
+        debugCanvas_ = canvas.GetComponent<DebugViewer>();
+        debugCanvas_.gameObject.SetActive(false);
+
+        StartCoroutine("CheckDebugViewer");
     }
 
 	public void GameRestart()
     {
         Application.LoadLevel(nowSceneName);
+    }
+
+    //! デバッグメニューの更新処理
+    
+    IEnumerator CheckDebugViewer()
+    {
+        const float triggerTime = 1.0f;
+
+        float wheelPushedTime = 0.0f;
+        while (true)    
+        {
+
+            if (Input.GetMouseButton(2))
+            {
+                wheelPushedTime += Time.deltaTime;
+            }
+
+            if (Input.GetMouseButtonUp(2))
+            {
+                wheelPushedTime = 0.0f;
+            }
+            
+            if (wheelPushedTime > triggerTime)
+            {
+                debugCanvas_.InvertActive();
+                wheelPushedTime = 0.0f;
+            }
+            
+            yield return null;
+        }
     }
 }
---
+++ Katana/Assets/Scripts/UI/Debug/DebugScrollController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DebugScrollController : MonoBehaviour 
+{    
+    [SerializeField]
+	public RectTransform prefab = null;
+
+	void Start () 
+	{
+		for(int i=0; i<0; i++)
+		{
+			var item = GameObject.Instantiate(prefab) as RectTransform;
+			item.SetParent(transform, false);
+
+			var text = item.GetComponentInChildren<Text>();
+			text.text = "item:" + i.ToString();
+		}
+	}
+}
