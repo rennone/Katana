83e405246aca0cebd3e26ae3509456297d9ecd50
Yuki Kono <yuboyubo919@gmail.com>
2016-02-15 22:33:11 +0900

    画面サイズに応じてUIのサイズと位置を自動調整する仕組みを導入（縦横比は16:9にしてる）

---
+++ Katana/Assets/Scripts/UI/UI_SizeController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UI_SizeController : MonoBehaviour {
+
+    //とりあえず1920*1080（16:9）を基準としてる
+    const float BASE_WIDTH = 1920;
+    const float BASE_HEIGHT = 1080;
+
+    RectTransform thisRect;
+    Vector3 firstScale;
+    Vector3 firstPos;
+
+	void Start () {
+        thisRect = GetComponent<RectTransform>();
+        firstScale = thisRect.localScale;
+        firstPos = thisRect.localPosition;
+
+        FitWindow();
+	}
+
+    void FitWindow()
+    {
+        float scaleRatio = Screen.width/BASE_WIDTH;
+        thisRect.localScale = firstScale * scaleRatio;
+        thisRect.localPosition = firstPos * scaleRatio;
+    }
+}
