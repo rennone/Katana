054f274355018eb9c970176399ed254867df18bd
Yuki Kono <yuboyubo919@gmail.com>
2016-05-15 19:09:55 +0900

    動く背景の大枠を実装

---
+++ Katana/Assets/Scripts/BackGround/RandomUVScroller.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomUVScroller : MonoBehaviour {
+
+    [SerializeField]
+    float minScrollSpeed = 0.1f;
+
+    [SerializeField]
+    float maxScrollSpeed = 0.2f;
+
+    [SerializeField]
+    private float repeatDistance = 1f;
+
+    Material thisMat = null;
+
+    void Start()
+    {
+        thisMat = this.GetComponent<MeshRenderer>().material; GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", Vector2.zero);
+        thisMat.SetTextureOffset("_MainTex", Vector2.zero);
+        nowScrollSpeed = Random.Range(minScrollSpeed, maxScrollSpeed);
+        nowScrollDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+    }
+
+    Vector2 nowScrollSize = Vector2.zero;
+    float nowScrollSpeed = 0;
+    Vector2 nowScrollDirection = Vector2.zero;
+    float scrollDistance = 0;
+    void Update()
+    {
+        nowScrollSize += nowScrollDirection * nowScrollSpeed * Time.deltaTime;
+
+        if(nowScrollSize.magnitude >= repeatDistance)
+        {
+            nowScrollDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+            nowScrollSpeed = Random.Range(minScrollSpeed, maxScrollSpeed);
+            scrollDistance = 0;
+        }
+
+        thisMat.SetTextureOffset("_MainTex", nowScrollSize);
+    }
+}
---
+++ Katana/Assets/Scripts/BackGround/RepeatUVScroller.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RepeatUVScroller : MonoBehaviour {
+
+    [SerializeField]
+    Vector2 scrollDirection = Vector2.zero;
+
+    [SerializeField]
+    float scrollSpeed = 0.1f;
+
+    [SerializeField]
+    private float repeatDistance = 1f;
+
+    Material thisMat = null;
+
+    void Start()
+    {
+        thisMat = this.GetComponent<MeshRenderer>().material; GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", Vector2.zero);
+        thisMat.SetTextureOffset("_MainTex", Vector2.zero);
+    }
+
+    Vector2 nowScrollSize = Vector2.zero;
+    float scrollDistance = 0;
+    void Update()
+    {
+        nowScrollSize += scrollDirection * scrollSpeed * Time.deltaTime;
+
+        if(nowScrollSize.magnitude >= repeatDistance)
+        {
+            scrollDirection *= -1;
+            scrollDistance = 0;
+        }
+
+        thisMat.SetTextureOffset("_MainTex", nowScrollSize);
+    }
+}
