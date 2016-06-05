e4aec0c495377d8140e3c46834a6164e21e08591
nashik <nashiko1215@gmail.com>
2016-06-05 17:16:28 +0900

    下方向からのみすり抜ける床を作成

---
+++ Katana/Assets/Scripts/Actor/Platforms/Move/OneTimeCollisionIgnorer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OneTimeCollisionIgnorer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("enter");
+        Physics.IgnoreCollision(collider.gameObject.GetComponent<Collider>(), GetComponent<Collider>(), true);
+    }
+    void OnTriggerExit(Collider collider)
+    {
+        Debug.Log("exit");
+        Physics.IgnoreCollision(collider.gameObject.GetComponent<Collider>(), GetComponent<Collider>(), false);
+    }
+}
